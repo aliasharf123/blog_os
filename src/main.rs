b8e4e5d3b527f240b781d29d298f5ed6b2a85fc9
aliashrafali770@gmail.com <aliashrafali770@gmail.com>
2024-03-24 01:23:14 +0200

    double-fault-exceptions

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 #![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
+pub mod gdt;
 pub mod interrupts;
 pub mod vga_buffer;
 
@@ -18,11 +19,19 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
-    interrupts::init_idt();
-
+    init();
     // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3(); // new
+    fn stack_overflow() {
+        stack_overflow(); // for each recursion, the return address is pushed
+    }
+
+    // trigger a stack overflow
+    stack_overflow();
 
     println!("It did not crash!");
     loop {}
+}
+pub fn init() {
+    gdt::init();
+    interrupts::init_idt();
 }
