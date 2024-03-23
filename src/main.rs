06feecb37db76188e003661cd2781a628bf234b9
aliashrafali770@gmail.com <aliashrafali770@gmail.com>
2024-03-24 00:05:10 +0200

    CPU Exceptions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
-mod vga_buffer;
+pub mod interrupts;
+pub mod vga_buffer;
 
 /// This function is called on panic.
 #[panic_handler]
@@ -14,7 +16,13 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    panic!("some panic message");
+    println!("Hello World{}", "!");
 
+    interrupts::init_idt();
+
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3(); // new
+
+    println!("It did not crash!");
     loop {}
 }
