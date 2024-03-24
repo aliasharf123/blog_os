b71ce4be899fca6908d9a15ffd85c1cf54126272
aliashrafali770@gmail.com <aliashrafali770@gmail.com>
2024-03-24 05:35:47 +0200

    Hardware Interrupts (keyboard, timer)

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ pub mod vga_buffer;
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
 
-    loop {}
+    hlt_loop();
 }
 
 #[no_mangle] // don't mangle the name of this function
@@ -20,18 +20,19 @@ pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
     init();
-    // invoke a breakpoint exception
-    fn stack_overflow() {
-        stack_overflow(); // for each recursion, the return address is pushed
-    }
-
-    // trigger a stack overflow
-    stack_overflow();
 
     println!("It did not crash!");
-    loop {}
+    hlt_loop();
 }
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
+}
+// The hlt Instruction: https://os.phil-opp.com/hardware-interrupts/#the-hlt-instruction
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
