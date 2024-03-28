3ff296a4fc22514a5e5145c8b18bbc8cee664adc
aliashrafali770@gmail.com <aliashrafali770@gmail.com>
2024-03-28 03:51:42 +0200

    allocator-designs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 #![feature(abi_x86_interrupt)]
-
+#![feature(const_mut_refs)]
 use crate::memory::BootInfoFrameAllocator;
 use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 use bootloader::{entry_point, BootInfo};
